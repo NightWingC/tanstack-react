9ba410250bf10a960c5146be439d9e4d089b5df1
Christian <christian@dreamsengine.io>
2022-11-28 14:04:08 -0600

    TanStack

--- 01-cripto-random/src/App.tsx
+++ 01-cripto-random/src/App.tsx
@@ -1,45 +1,22 @@
-import { useEffect, useReducer, useState } from 'react';
 import './App.css';
+import { useRandom } from './hooks/useRandom';
 
-const getRandomNumberAPI = async ():Promise<number> => {
-  const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new');
-  const numberString = await res.text();
-
-  return +numberString;
-}
 
 export const App = () => {
-  const [number, setNumber] = useState<number>();
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string>();
-  const [key, forceRefresh] = useReducer((num) => num + 1, 0);
-
-  useEffect(() => {
-    setIsLoading(true);
-    getRandomNumberAPI()
-      .then( setNumber )
-      .catch( error => setError( error.message ));
-  }, [key]);
-
-  useEffect(() => {
-    if( number ) setIsLoading( false );
-  }, [number]);
-
-  useEffect(() => {
-    if( error ) setIsLoading(false);
-  }, [error]);
+  
+  const query = useRandom();
 
   return (
     <div className="App App-header">
-      { isLoading 
+      { query.isFetching 
         ? ( <h2>Cargando...</h2> ) 
-        : ( <h2>Número aleatorio: { number }</h2> )
+        : ( <h2>Número aleatorio: { query.data }</h2> )
       }
 
-      { !isLoading && error  && ( <h3>{ error }</h3> ) }
+      { !query.isLoading && query.isError  && ( <h3>{ `${ query.error }` }</h3> ) }
 
-      <button onClick={ forceRefresh } disabled={ isLoading }>
-        { isLoading ? '...' : 'Nuevo numero' }
+      <button onClick={ () => query.refetch() } disabled={ query.isFetching }>
+        { query.isFetching ? '...' : 'Nuevo numero' }
       </button>
       
     </div>
