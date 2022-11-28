d2fc472ff88f5fd27fb73de5d277107ec6535b9c
Christian <christian@dreamsengine.io>
2022-11-28 13:06:38 -0600

    initial commit

---
+++ 01-cripto-random/src/App.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useReducer, useState } from 'react';
+import './App.css';
+
+const getRandomNumberAPI = async ():Promise<number> => {
+  const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new');
+  const numberString = await res.text();
+
+  return +numberString;
+}
+
+export const App = () => {
+  const [number, setNumber] = useState<number>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>();
+  const [key, forceRefresh] = useReducer((num) => num + 1, 0);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getRandomNumberAPI()
+      .then( setNumber )
+      .catch( error => setError( error.message ));
+  }, [key]);
+
+  useEffect(() => {
+    if( number ) setIsLoading( false );
+  }, [number]);
+
+  useEffect(() => {
+    if( error ) setIsLoading(false);
+  }, [error]);
+
+  return (
+    <div className="App App-header">
+      { isLoading 
+        ? ( <h2>Cargando...</h2> ) 
+        : ( <h2>Número aleatorio: { number }</h2> )
+      }
+
+      { !isLoading && error  && ( <h3>{ error }</h3> ) }
+
+      <button onClick={ forceRefresh } disabled={ isLoading }>
+        { isLoading ? '...' : 'Nuevo numero' }
+      </button>
+      
+    </div>
+  );
+}
+
