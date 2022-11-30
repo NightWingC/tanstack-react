33fd13aa08c05d632bddb37dea58096ec8cd6712
Christian <christian@dreamsengine.io>
2022-11-29 19:36:58 -0600

    baerer

--- 02-react-query-issues/src/api/githubApi.ts
+++ 02-react-query-issues/src/api/githubApi.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const githubApi = axios.create({
     baseURL: 'https://api.github.com/repos/facebook/react',
     headers: {
-        Authorization: 'Bearer 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'
+        Authorization: 'Bearer '
     },
 })
